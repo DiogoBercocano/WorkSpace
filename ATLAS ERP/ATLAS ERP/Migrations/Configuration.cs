96f3658da7bec4aa6868aeef0018ea8768c1a590
DiogoBercocano <dg.bercocano06@gmail.com>
2026-03-11 21:09:51 -0300

    atualização ATLAS ERP

--- ATLAS ERP/ATLAS ERP/Migrations/Configuration.cs
+++ ATLAS ERP/ATLAS ERP/Migrations/Configuration.cs
@@ -1,9 +1,9 @@
 ﻿namespace ATLAS_ERP.Migrations
 {
     using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using ATLAS_ERP.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<ATLAS_ERP.Data.AtlasContext>
     {
@@ -14,10 +14,35 @@
 
         protected override void Seed(ATLAS_ERP.Data.AtlasContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            // EMPRESA PADRÃO
+            context.Empresas.AddOrUpdate(e => e.CNPJ,
+                new ATLAS_ERP.Models.Empresa
+                {
+                    EmpresaId = 1,
+                    Nome = "Atlas ERP",
+                    CNPJ = "00000000000000",
+                    Email = "contato@atlas.com",
+                    Ativa = true
+                }
+            );
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+            context.SaveChanges();
+
+            // USUÁRIO ADMIN
+            context.Usuarios.AddOrUpdate(u => u.Email,
+                new ATLAS_ERP.Models.Usuario
+                {
+                    UsuarioId = 1,
+                    Name = "Administrador",
+                    Email = "admin@atlas.com",
+                    SenhaHash = "123",
+                    Role = "Admin",
+                    Ativo = true,
+                    EmpresaId = 1
+                }
+            );
+
+            context.SaveChanges();
         }
     }
 }
